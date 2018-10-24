acae23774736d8da7d013128cb75beaf5d4cb470
Travis Reynolds <travisreynolds17@gmail.com>
2018-10-24 16:43:54 -0400

    Initial board set - no match check yet

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,23 @@
 // Basic match 3 game. We're emulating HuniePop's puzzles. ONLY THE PUZZLES.
 // 4 types of token, plus heart token and broken heart token.
-// board is 10 x 10.
+// board is 8x8.
 
 const board = document.querySelector(".board");
+let c = board.getContext("2d");
+const rows = 8;
+const cols = 8;
 
-boardLength = 10;
-boardWidth = 10;
+board.width = board.height = Math.floor(0.6 * window.innerWidth);
 
 let spaces = [];
+let tokens = [];
+// holds possible type of token. Later to be replaced with pictures?
+let tokenValues = ["green", "white", "red", "blue", "black", "pink"];
+
+let animating = true;
+let tileSize = Math.floor(board.width / rows);
+let offset = tileSize / 2;
+console.log(tileSize);
 
 function render(node, template) {
   // injects HTML at specified node
@@ -15,48 +25,70 @@ function render(node, template) {
 }
 
 function init() {
-  initBoard(boardLength, boardWidth);
-  setBoardValues();
-  renderBoard();
+  initBoard(cols, rows, tileSize);
 }
-function initBoard(length, width) {
+function initBoard(length, width, tileSize) {
   // set up all possible positions. This function does NOT assign values. It only creates the templates.
   let count = 0;
 
-  for (var i = 0; i < length; i++) {
-    for (var k = 0; k < width; k++) {
+  for (var i = 0; i < cols; i++) {
+    for (var k = 0; k < rows; k++) {
       // each space is an object. its properties will be used to tell us where it is and what's in it.
       spaces[count] = {
         index: count,
-        xPos: k,
-        yPos: i,
-        value: null,
-        active: false
+        x: k * tileSize + offset,
+        y: i * tileSize + offset,
+        value: "",
+        active: false,
+        size: tileSize / width
       };
       count++;
     }
   }
+  spaces = setBoardValues(spaces);
+  initTokens(spaces);
 }
 
-function setBoardValues() {
+function setBoardValues(array) {
   // assigns each space a numeric value between 0 and 5. These values represent game takens.
-  spaces.forEach(item => (item.value = getRandom(0, 6)));
+  array.forEach(item => (item.value = getRandom(0, 6)));
+  return array;
 }
 
-function renderBoard() {
-  // displays new game board's values
-  let injectable = "";
-  let token = "";
-  for (var i = 0; i < spaces.length; i++) {
-    token = spaces[i].value.toString();
-    injectable += "<div class = 'space'>\n" + token + "\n</div>\n";
-    console.log(injectable);
+function initTokens(array) {
+  for (let i = 0; i < rows * cols; i++) {
+    tokens[i] = new Token(
+      array[i].x,
+      array[i].y,
+      2,
+      2,
+      array[i].size,
+      tokenValues[array[i].value]
+    );
   }
-  render(board, injectable);
+  tokens.forEach(item => item.draw());
 }
+
 function getRandom(min, max) {
   // note: max is not inclusive because of floor method. make max 1 more than you actually want to include.
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function Token(x, y, dx, dy, radius, value) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.value = value;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.value;
+    c.stroke();
+    c.fill();
+  };
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+
+// logs message
+
+gulp.task("message", function() {
+  return console.log("Gulp is running");
+});
+
+gulp.task("default", function() {
+  return console.log("Gulp is running");
+});
