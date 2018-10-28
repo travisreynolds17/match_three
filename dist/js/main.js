02bd854bf3809b72aaef9dd3c3c20ceead4b273b
Travis Reynolds <travisreynolds17@gmail.com>
2018-10-28 01:52:17 -0400

    added mouse x/y detection

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@
 
 const board = document.querySelector(".board");
 let c = board.getContext("2d");
+const resetBtn = document.querySelector(".reset");
 const rows = 8;
 const cols = 8;
 c.font = "48px serif";
@@ -28,9 +29,13 @@ let tokenValues = [
 let animating = true;
 let tileSize = Math.floor(board.width / rows);
 let offset = tileSize / 2;
+let tempX = [];
+let mousePos = ""; // will hold x and y position of mouse cursor
 
 let boardReady = false; // to become true when a board is set up that has no immediate matches
 let falseBoards = 0; // tests number pre-game boards that have a match
+
+resetBtn.addEventListener("click", init);
 
 function render(node, template) {
   // injects HTML at specified node
@@ -71,70 +76,36 @@ function initBoard(length, width, tileSize) {
   }
 }
 
-// I know this functin is horrible. We will break it down at some point. I'm convinced I can push all these whiles into a foor loop.
-//
+// seems to work. This function takes the initial array of values and creates a new one where each horizontal row
+// is concatenated and placed in the corresponding index of a new array. This is for testing purposes.
+// the function is recursive and will call itself until every value is handled
+function getRows(min, max, array, iterator, recursiveCounter, tempArray) {
+  let temp = "";
+  let i = iterator;
+  let counter = recursiveCounter;
+  let arrayClone = array;
+  let temp2 = tempArray;
+  while (i >= min && i < max) {
+    temp += array[i].value.toString();
+
+    i++;
+  }
+
+  temp2[counter] = temp;
+  counter++;
+
+  if (i >= rows * cols) {
+    console.log("hi");
+    tempX = temp2;
+  } else {
+    getRows(min + rows, max + rows, arrayClone, i, counter, temp2);
+  }
+}
+
 function testValues(array) {
-  let i = 0;
-  temp = "";
-  tempX = [];
+  getRows(0, rows, array, 0, 0, []);
+
   let tempY = [];
-
-  // While loops will take each space's value and concatenate it into 8 strings, which we will test later.
-  while (i < 8) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[0] = temp;
-  temp = "";
-
-  while (i >= 8 && i < 16) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[1] = temp;
-  temp = "";
-
-  while (i >= 16 && i < 24) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[2] = temp;
-  temp = "";
-
-  while (i >= 24 && i < 32) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[3] = temp;
-  temp = "";
-
-  while (i >= 32 && i < 40) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[4] = temp;
-  temp = "";
-
-  while (i >= 40 && i < 48) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[5] = temp;
-  temp = "";
-
-  while (i >= 48 && i < 56) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[6] = temp;
-  temp = "";
-
-  while (i >= 56 && i < 64) {
-    temp += array[i].value.toString();
-    i++;
-  }
-  tempX[7] = temp;
-  temp = "";
 
   // get y values
   let j = "";
@@ -160,8 +131,8 @@ function testValues(array) {
     //test for three consecutive repeating values (a match)
     for (let k = 0; k < temp.length; k++) {
       if (temp[k] == temp[k + 1] && temp[k] == temp[k + 2]) {
-        console.log("horiz match found at");
-        console.log(i, k);
+        // console.log("horiz match found at");
+        // console.log(i, k);
         state = true;
       }
     }
@@ -175,8 +146,8 @@ function testValues(array) {
     //test for three consecutive repeating values (a match)
     for (let k = 0; k < temp.length; k++) {
       if (temp[k] == temp[k + 1] && temp[k] == temp[k + 2]) {
-        console.log("vert match found at");
-        console.log(i, k);
+        // console.log("vert match found at");
+        // console.log(i, k);
         state = true;
       }
     }
@@ -195,13 +166,14 @@ function setBoardValues(array) {
 function initTokens(array) {
   for (let i = 0; i < rows * cols; i++) {
     tokens[i] = new Token(
+      i,
       array[i].x,
       array[i].y,
       array[i].row,
       array[i].col,
       2,
       2,
-      array[i].size,
+      array[i].size * 2,
       tokenValues[array[i].value]
     );
   }
@@ -213,7 +185,8 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function Token(x, y, row, col, dx, dy, radius, value) {
+function Token(index, x, y, row, col, dx, dy, radius, value) {
+  this.index = index;
   this.x = x;
   this.y = y;
   this.dx = dx;
@@ -222,15 +195,54 @@ function Token(x, y, row, col, dx, dy, radius, value) {
   this.value = value;
   this.row = row;
   this.col = col;
+  this.hitX = x + radius; // these determine the clickable area of the token
+  this.hitY = y + radius;
 
-  this.draw = function() {
+  this.draw = function(color) {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.value;
+    c.fillStyle = color || this.value;
     c.stroke();
     c.fill();
   };
 }
 
+function CanvasState() {}
+
+// tracking mouse position
+function getMousePosition(board, event) {
+  let box = board.getBoundingClientRect();
+  return {
+    x: Math.floor(event.clientX - box.left),
+    y: Math.floor(event.clientY - box.top)
+  };
+}
+
+// mousePos variable will have a .x and .y property
+board.addEventListener("mousemove", function(event) {
+  mousePos = getMousePosition(board, event);
+});
+
+// find out which token we clicked
+
+board.addEventListener("mousedown", clickCheck);
+
+function clickCheck() {
+  let x = mousePos.x;
+  let y = mousePos.y;
+
+  let test = tokens.slice();
+
+  // loop through array of tokens to find which one was clicked
+  for (let i = 0; i < test.length; i++) {
+    let e = test[i]; // for brevity
+    let set = e.radius; // this represents the offset, because each circle is a friggin circle
+
+    // test to see if current mouse point if within its x/y boundaries
+    if (e.x < x + set && e.y < y + set && (e.hitX > x && e.hitY > y)) {
+      e.draw("violet");
+    }
+  }
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-init();
